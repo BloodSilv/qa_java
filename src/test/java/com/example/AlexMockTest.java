4526c0fb15997fc9a8c8c96f8d5eaf9df03b3a4c
chvn <chvn@live.ru>
2022-04-05 20:55:39 +0500

    Second commit for qa_java_Sprint_2

--- src/test/java/com/example/AlexMockTest.java
+++ src/test/java/com/example/AlexMockTest.java
@@ -6,28 +6,39 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 @RunWith(MockitoJUnitRunner.class)
 public class AlexMockTest {
+
     @Mock
-    Alex alex;
+    Feline feline;
 
     @Test
-    public void getFriends() {
-        alex.getFriends();
-        Mockito.verify(alex).getFriends();
+    public void getFriendsForAlex() throws Exception {
+        Alex alex = new Alex(this.feline);
+        List<String> actual = alex.getFriends();
+        List<String> expected = Arrays.asList("Марти", "Глория", "Мелман");
+        assertEquals(expected,actual);
     }
 
     @Test
-    public void getPlaceOfLiving() {
-        alex.getPlaceOfLiving();
-        Mockito.verify(alex, Mockito.only()).getPlaceOfLiving();
+    public void getPlaceOfLivingForAlex() throws Exception {
+        Alex alex = new Alex(this.feline);
+        String actual = alex.getPlaceOfLiving();
+        String expected = "Нью-Йоркский зоопарк";
+        assertEquals(expected,actual);
     }
 
     @Test
-    public void getKittens() {
-        alex.getKittens();
-        alex.getKittens();
-        alex.getKittens();
-        Mockito.verify(alex, Mockito.times(3)).getKittens();
+    public void getKittensForAlexZero() throws Exception {
+        Alex alex = new Alex(this.feline);
+        Mockito.when(feline.getKittens()).thenReturn(0);
+        int actual = alex.getKittens();
+        int expected = feline.getKittens();
+        assertEquals(expected,actual);
     }
 }
